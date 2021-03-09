57ef7c95647ef4ab2617064de474add00393e9d6
Jeremy Martin <jeremy.martin@smart-chain.fr>
2021-03-09 14:35:43 +0100

    add transaction

--- readme.md
+++ readme.md
@@ -4,6 +4,15 @@ install https://github.com/algorand/sandbox
 
 npm install algosdk
 
+
+
+
+
+* See account list
+
+./sandbox goal account list
+
+
 * Export private key 
 
 ./sandbox goal account export -a LBLP7MHECZ5FL2JVRCDPLXN25WW3CLQLHDLSAUWNV4CGYAHYBHKNY2WNS4
--- start.js
+++ start.js
@@ -32,7 +32,7 @@ let algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
 
 
 
-const passphrase = "settle nominee cash crazy tenant build unfair emotion earth column canvas right pass harbor jungle resource wine ginger pyramid where buffalo object need abstract waste";
+const passphrase = "curtain pelican because start banner zebra tag harbor august flee scene dolphin beef snack vacuum choice palace leave parent find dose require silent ability rule";
 
 let myAccount = algosdk.mnemonicToSecretKey(passphrase);
 console.log("My address: %s", myAccount.addr);
@@ -73,3 +73,76 @@ console.log("My address: %s", myAccount.addr);
 });
 
 
+
+//send 1 Algo to the sink
+(async () => {
+    let params = await algodClient.getTransactionParams().do();
+    // comment out the next two lines to use suggested fee
+    params.fee = 1000;
+    params.flatFee = true;
+    const receiver = "GD64YIY3TWGDMCNPP553DZPPR6LDUSFQOIJVFDPPXWEG3FVOJCCDBBHU5A";
+    let note = algosdk.encodeObj("Hello World");
+    let txn = algosdk.makePaymentTxnWithSuggestedParams(myAccount.addr, receiver, 1000000, undefined, note, params);        
+    //sign the transaction
+    let signedTxn = txn.signTxn(myAccount.sk);
+    let txId = txn.txID().toString();
+    console.log("Signed transaction with txID: %s", txId);
+    //submit the transaction
+    await algodClient.sendRawTransaction(signedTxn).do();
+
+
+    /**
+     * utility function to wait on a transaction to be confirmed
+     * the timeout parameter indicates how many rounds do you wish to check pending transactions for
+     */
+    const waitForConfirmation = async function (algodclient, txId, timeout) {
+        // Wait until the transaction is confirmed or rejected, or until 'timeout'
+        // number of rounds have passed.
+        //     Args:
+        // txId(str): the transaction to wait for
+        // timeout(int): maximum number of rounds to wait
+        // Returns:
+        // pending transaction information, or throws an error if the transaction
+        // is not confirmed or rejected in the next timeout rounds
+        if (algodclient == null || txId == null || timeout < 0) {
+            throw "Bad arguments.";
+        }
+        let status = (await algodclient.status().do());
+        if (status == undefined) throw new Error("Unable to get node status");
+        let startround = status["last-round"] + 1;   
+        let currentround = startround;
+
+        while (currentround < (startround + timeout)) {
+            let pendingInfo = await algodclient.pendingTransactionInformation(txId).do();      
+            if (pendingInfo != undefined) {
+                if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+                    //Got the completed Transaction
+                    return pendingInfo;
+                }
+                else {
+                    if (pendingInfo["pool-error"] != null && pendingInfo["pool-error"].length > 0) {
+                        // If there was a pool error, then the transaction has been rejected!
+                        throw new Error("Transaction Rejected" + " pool error" + pendingInfo["pool-error"]);
+                    }
+                }
+            } 
+            await algodclient.statusAfterBlock(currentround).do();
+            currentround++;
+        }
+        throw new Error("Transaction not confirmed after " + timeout + " rounds!");
+    };
+
+
+
+
+    // Wait for confirmation
+    let confirmedTxn = await waitForConfirmation(algodClient, txId, 4);
+    //Get the completed Transaction
+    console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+    let mytxinfo = JSON.stringify(confirmedTxn.txn.txn, undefined, 2);
+    console.log("Transaction information: %o", mytxinfo);
+    var string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
+    console.log("Note field: ", string);
+})().catch(e => {
+    console.log(e);
+});
