8debd0964485209adcb784bcd7d9612aa2fdc3b0
Dann <dannba0@gmail.com>
2021-03-30 10:45:51 +0200

    ajout AssetCreate2.0

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-   path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+   path: path.resolve(__dirname, process.env.NODE_ENV +'.env')
 });
 
 
