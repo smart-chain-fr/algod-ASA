6817f23f35412d956310752f23ba9e00001db7e7
jules-xnb <70725850+jules-xnb@users.noreply.github.com>
2021-03-31 17:28:30 +0200

    Delete test.js

--- test.js
+++
@@ -1,4 +0,0 @@
-//test 
-// see the possibility to import the function
-const tokenCreate = require("./AssetCreate2.0.js");
-tokenCreate.createAsset("KVDWUPF7WSYHFFMNQGCEGFVGARWV55GVINLGLEFEWF7WC3NTQ5SQM3FSUY", 1000000, 2, "test", "TST", "test.com").catch(console.error);
