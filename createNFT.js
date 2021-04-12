51732814c56fcebbd50d88404d9784479b340d64
Dann <dannba0@gmail.com>
2021-04-12 15:13:47 +0200

    createNFT function

---
+++ createNFT.js
@@ -0,0 +1,103 @@
+const config = require('./config/config.js');
+const algosdk = require('algosdk');
+const utils = require('./utils')
+
+/**
+ * Create an asset 
+ * TODO : export NODE_ENV=source
+ * @param {string} secret_key - memonic of the sender 
+ * @param {string} assetName - NFT name
+ * @param {string} unitName - symbol of the NFT
+ * @param {string} NFTURL - website url
+ * @return {Object} NFT ID / Transaction ID 
+*/
+async function createNFT(secret_key, assetName, unitName, NFTURL) {
+
+    if(argumentsVerification(secret_key, assetName, unitName,NFTURL) === 1){
+
+        try {
+            utils.retrieveBaseConfig()
+        } catch (e) {
+            console.error(e)
+            return
+        }
+
+        const algodToken = config.ALGOD_TOKEN;
+        const algodServer = config.ALGOD_SERVER; 
+        const algodPort = config.ALGOD_PORT;
+
+        let algodclient = new algosdk.Algodv2(algodToken, algodServer, algodPort); 
+
+        // Account recovery 
+        const sender = algosdk.mnemonicToSecretKey(secret_key)
+        if (!algosdk.isValidAddress(sender.addr)){
+            console.log("Error : Bad secret_key")
+            return 
+        }
+    
+        // Define params
+        const suggestedParams = await algodclient.getTransactionParams().do();
+        const defaultFrozen = false; // whether accounts should be frozen by default
+        total = 1
+        decimals = 0
+    
+        // Create the asset creation transaction
+        const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+    
+            suggestedParams,
+            from: sender.addr, 
+            manager: sender.addr,
+            
+            total,
+            decimals,
+            assetName,
+            unitName,
+            NFTURL,
+    
+        });
+        
+        // Sign the transaction
+        const signedTxn = txn.signTxn(sender.sk);
+
+        // Submit the transaction
+        let tx = await algodclient.sendRawTransaction(signedTxn).do();
+    
+        // Wait for confirmation
+        let confirmedTxn = await utils.waitForConfirmation(algodclient, tx.txId, 3);
+        // console.log("Transaction : " + tx.txId + "\nConfirmed in round " + confirmedTxn["confirmed-round"]);
+    
+        // Get the ASA-ID 
+        let ptx = await algodclient.pendingTransactionInformation(tx.txId).do();
+        NFT_ID = ptx["asset-index"];
+        //console.log("NFT_ID = " + NFT_ID);
+    
+        return {
+            "txId": tx.txId, 
+            "NFT_ID": NFT_ID,
+            "confirmed round": confirmedTxn['confirmed-round']
+        }
+    }
+}
+
+function argumentsVerification(secret_key, assetName, unitName,NFTURL){
+    if (
+        typeof secret_key == 'string' && 
+        typeof assetName == 'string' && 
+        typeof unitName == 'string' && 
+        typeof NFTURL == 'string'
+    ){
+        return 1
+    }
+    console.log("Error : Bad Arguments")
+    return 0
+}
+
+module.exports = {
+    createNFT,
+};
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const createAccount = require("./createAccount");
 const createASA = require("./createASA");
 const atomic = require('./atomic')
+const createNFT = require("./createNFT")
+
 
 async function main() {
     let account = createAccount.createAccount(); 
     console.log("account ",account)
 
     //use your mnemonic
-    let sk = "rescue stock blade work vanish neck expire borrow dolphin insane immense awesome depth tree bamboo uniform direct fold tortoise mass remember next punch ability vicious"
-    let newASA = await createASA.createASA(sk, "TEST", "TST", 210000000, 1, "test..");
+    let sk = "exchange fat eye height amused peasant bread snap state author warm combine sock long quarter balance travel true stove bicycle else remind vendor absorb laugh"
+    let newASA = await createASA.createASA(sk, "BOBBY", "BBY", 20000000, 1, "test..");
     console.log("new ASA ",newASA)
 
     let atomicTransfer = await atomic.atomic(sk,account,newASA.ASA_ID,10)
     console.log("atomic transfer ",atomicTransfer)
+
+    let newNFT = await createNFT.createNFT(sk,"NFTTEST","NFTT","url")
+    console.log("new NFT  ",newNFT)
+
+
+    let atomicTransferNFT = await atomic.atomic(sk,account,newNFT.NFT_ID,1)
+    console.log("atomic transfer nft ",atomicTransferNFT)
+
 }
 
 main()
