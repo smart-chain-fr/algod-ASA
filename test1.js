21ddb1e48f5f026a6112312306f252fee974045a
Dann <dannba0@gmail.com>
2021-04-20 09:57:32 +0200

    random teal code

--- test1.js
+++ test1.js
@@ -24,7 +24,7 @@ const fs = require('fs');
     // create logic sig
     var fs = require('fs'),
         path = require('path'),
-        filePath = path.join(__dirname, 'samplearg.teal');
+        filePath = path.join(__dirname, 'samplesss.teal');
         // filePath = path.join(__dirname, '<PLACEHOLDER>');       
     let data = fs.readFileSync(filePath);
     let results = await algodclient.compile(data).do();
@@ -36,31 +36,39 @@ const fs = require('fs');
     //let lsig = algosdk.makeLogicSig(program);
 
     // String parameter
-    //let args = [["mystring"]];
+    // let args = [123];
     // let lsig = algosdk.makeLogicSig(program, args);
     // Integer parameter
-    // let args = [new Uint8Array([123])];
-    console.log("args : \n",args);
-
+    //const buffer = Buffer.alloc(8);
+    //const bigIntValue = BigInt(123);
+    //buffer.writeBigUInt64BE(bigIntValue)
+    //let arg1 = Uint8Array.from(buffer);
+    //let args = [algosdk.encodeUint64(123)];
+    args = ["123"] //[new Uint8Array(Buffer.from("123", 'base64'))];
+    console.log(args)
+    let lsig = algosdk.makeLogicSig(program);
+    //let args = [new Uint8Array([126])];
     console.log(program)
-    let lsig = algosdk.makeLogicSig(program,args);
+    //let lsig = algosdk.makeLogicSig(program, args);
     console.log("lsig : " + lsig.address());   
 
-    // create a transaction  
+    // create a transaction
     let sender = lsig.address();
     let receiver = "GZO2IAG7QI5WVL4HWLLGPS4QADW2EPQVEZIGLFXCTYPYEKTHV2CNPUYNA4";
-    let amount = 10000;
+    let amount = 1;
     let closeToRemaninder = undefined;
     let note = undefined;
+    console.log('sqdjdqlsdkjlqsdjlq', params)
     let txn = algosdk.makePaymentTxnWithSuggestedParams(sender, receiver, amount, closeToRemaninder, note, params)
 
     let rawSignedTxn = algosdk.signLogicSigTransactionObject(txn, lsig);
 
     // send raw LogicSigTransaction to network
-    let tx = (await algodclient.sendRawTransaction(rawSignedTxn.blob).do());
+    console.log('blob ', rawSignedTxn.blob)
+    let tx = (await algodclient.sendRawTransaction(rawSignedTxn.blob).do().catch(e=>{console.log('HERE', e)}));
     console.log("Transaction : " + tx.txId);   
     console.log(await utils.waitForConfirmation(algodclient, tx.txId,3));
 
 })().catch(e => {
-    console.log('catch', e);
+    console.log('catch1', e);
 });
